8cfff9deae140f9640b630f46b33ffd9ed69ad63
weedsim <109517699+weedsim@users.noreply.github.com>
2026-08-24 22:09:34 +0900

    Rebuild blog with Astro (AstroPaper v6)

---
+++ astro-paper.config.ts
@@ -0,0 +1,41 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://weedsim.github.io/",
+    title: "HY Devlog",
+    description: "다다익선 개발자의 기술 블로그 겸 포트폴리오",
+    author: "Sim HoYeon",
+    profile: "https://github.com/weedsim",
+    ogImage: "default-og.jpg",
+    lang: "ko",
+    timezone: "Asia/Seoul",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: false,
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github",   url: "https://github.com/weedsim" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
